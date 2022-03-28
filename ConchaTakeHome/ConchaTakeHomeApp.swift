217754042e706084a72f5049676d9d711aace65d
Amr Al-Refae <amrezo94@gmail.com>
2022-03-28 15:07:43 -0400

    Initial Commit

---
+++ ConchaTakeHome/ConchaTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConchaTakeHomeApp.swift
+//  ConchaTakeHome
+//
+//  Created by Amr Al-Refae on 2022-03-28.
+//
+
+import SwiftUI
+
+@main
+struct ConchaTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
